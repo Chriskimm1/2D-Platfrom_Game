294b8bf276ab7283569037c87ad817ab21c25cb8
chris kim <kchris81@gmail.com>
2015-04-21 09:41:02 -0400

    updated throwing shuriken

--- public/js/game/game.js
+++ public/js/game/game.js
@@ -1,7 +1,10 @@
 var game = new Phaser.Game(1200, 640, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
 var platforms;
 var map;
 var shurikenTime = 0;
+var shuriken;
+
 
 //loads up all the images and sprites
 function preload() {
@@ -9,8 +12,8 @@ game.load.tilemap("levelone", "js/assets/graveyard.json", null, Phaser.Tilemap.T
 /*game.load.image("background", "js/assets/background.png");*/
 game.load.image("tiles", "js/assets/Graveyard.png");
 game.load.spritesheet("dude", "js/assets/dude.png", 58, 80);
-game.load.image("shuriken", "js/assets/shuriken.png");
-game.load.spritesheet("baddude", "js/assets/badguy.png" 48,48);
+game.load.image("shuriken", "js/assets/shuriken.PNG");
+game.load.spritesheet("baddude", "js/assets/badguy.png", 48,48);
 
 }
 //defining platfrom
@@ -43,16 +46,31 @@ function create() {
     player.body.gravity.y = 300;
     player.body.collideWorldBounds = true;
 
-    // two animations, walking left and right.
+    //Animation frames for player
     player.animations.add('left', [4, 8, 11, 3], 16, true);
     player.animations.add('right', [4, 8, 11, 3], 16, true);
     player.animations.add('up', [1], 1, true);
+    player.animations.add("shoot", [15], 1, true);
+
+    shuriken = game.add.group();
+    shuriken.enableBody = true;
+    shuriken.physicsBodyType = Phaser.Physics.ARCADE;
+    shuriken.createMultiple(30, 'shuriken');
+    shuriken.setAll('anchor.x', 0.5);
+    shuriken.setAll('anchor.y', 1);
+    shuriken.setAll('outOfBoundsKill', true);
+    shuriken.setAll('checkWorldBounds', true);
+
+
 }
 
 
 function update() {
 	 game.physics.arcade.collide(player, collide);
+	 //allows arrow keypress
 	 cursors = game.input.keyboard.createCursorKeys();
+	 //defines space key
+	 spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 	 player.body.velocity.x = 0;
 
     if (cursors.left.isDown)
@@ -63,6 +81,7 @@ function update() {
         player.scale.setTo(-0.7,0.7)
         player.body.velocity.x = -130;
         player.animations.play('left');
+        facing = "left"
     }
     else if (cursors.right.isDown)
     {
@@ -70,6 +89,7 @@ function update() {
         player.scale.setTo(0.7,0.7)
         player.body.velocity.x = 130;
         player.animations.play('right');
+        facing = "right"
     }
     else
     {
@@ -86,4 +106,33 @@ function update() {
     {
     	player.animations.play('up')
     }
+    else if (spaceKey.isDown) 
+    {
+    		player.animations.play("shoot");
+        fireShuriken();
+    }
 }
+
+
+function fireShuriken() {
+    //  To avoid them being allowed to fire too fast we set a time limit
+    if (game.time.now > shurikenTime)
+    {
+        //  Grab the first shuriken we can from the pool
+        shuriken = shuriken.getFirstExists(false);
+        console.log(shuriken)
+        if (shuriken)
+        {
+            //  And fire it
+            shuriken.reset(player.x, player.y + 8);
+            shuriken.body.velocity.x = 400;
+            shurikenTime = game.time.now + 200;
+        }
+
+    }
+
+}
+
+
+
+
